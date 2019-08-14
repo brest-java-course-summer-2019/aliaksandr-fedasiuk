400f9d00502244348077b277ccc879734047895f
berdachuk <berdachuk@berdaflex.com>
2019-08-14 16:45:47 +0300

    delete department

--- human-resources/web-app/src/main/java/com/epam/brest/summer/courses2019/web_app/DepartmentController.java
+++ human-resources/web-app/src/main/java/com/epam/brest/summer/courses2019/web_app/DepartmentController.java
@@ -91,4 +91,16 @@ public class DepartmentController {
         this.departmentService.add(department);
         return "redirect:/departments";
     }
+
+    /**
+     * Delete department.
+     *
+     * @return view name
+     */
+    @GetMapping(value = "/department/{id}/delete")
+    public final String deleteDepartmentById(@PathVariable Integer id, Model model) {
+        LOGGER.debug("delete({},{})", id, model);
+        departmentService.delete(id);
+        return "redirect:/departments";
+    }
 }
