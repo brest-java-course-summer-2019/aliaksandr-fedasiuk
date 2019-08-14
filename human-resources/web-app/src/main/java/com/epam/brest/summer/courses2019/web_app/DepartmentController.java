2588a05a4a8c657338a212388f15d09d2d5f5eda
berdachuk <berdachuk@berdaflex.com>
2019-08-14 15:40:35 +0300

    edit department

--- human-resources/web-app/src/main/java/com/epam/brest/summer/courses2019/web_app/DepartmentController.java
+++ human-resources/web-app/src/main/java/com/epam/brest/summer/courses2019/web_app/DepartmentController.java
@@ -1,5 +1,6 @@
 package com.epam.brest.summer.courses2019.web_app;
 
+import com.epam.brest.summer.courses2019.model.Department;
 import com.epam.brest.summer.courses2019.service.DepartmentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * Department controller.
@@ -31,6 +33,20 @@ public class DepartmentController {
         LOGGER.debug("findAll({})", model);
         model.addAttribute("departments", departmentService.findAllWithAvgSalary());
         return "departments";
+    }
+
+    /**
+     * Goto edit department page.
+     *
+     * @return view name
+     */
+    @GetMapping(value = "/department/{id}")
+    public final String gotoEditDepartmentPage(@PathVariable Integer id, Model model) {
+
+        LOGGER.debug("gotoEditDepartmentPage({},{})", id, model);
+        Department department = departmentService.findById(id);
+        model.addAttribute("department", department);
+        return "department";
     }
 
     /**
